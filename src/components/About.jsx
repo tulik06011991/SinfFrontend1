0ce1d9082962c66a8da3ac5713bc323bd9a4c524
tulik <tolqinmirsaliyev@gmail.com>
2024-10-12 11:30:45 -0500

    sss

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div className="container mx-auto p-8">
             <h1 className="text-4xl font-bold mb-4">About Us</h1>
-            <p className="text-lg">
+            <p className="text-lg text-bold font-serif">
                 Men bu dastruni o'qituvchilar va o'quvchilar qulaylik bo'lishi uchun yaratdim  Bunda o'qituvchilar  biror fanga oid  test savollari va variantlari bor  word faylini yuklash orqali o'quvchilarga bilimlarni baholash uchun ishlatishadi M.Tulqin
             </p>
         </div>
