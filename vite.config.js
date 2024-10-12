0e4ac49cb55f33ea59b34f5c6bfbfb1dd7c51096
tulik <tolqinmirsaliyev@gmail.com>
2024-10-12 11:43:01 -0500

    sss

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['jwt-decode']
-  }
-})
+    include: ['jwt-decode'],
+  },
+  server: {
+    proxy: {
+      '/': {
+        target: 'https://sinfbackend2.onrender.com', // Backend URL manzilingiz
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
