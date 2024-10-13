835dd5d6b65c4d4bbdcfe5a7de81cfd3a860fdf1
tulik <tolqinmirsaliyev@gmail.com>
2024-10-13 01:29:11 -0500

    zaybal

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     
 
     const url = axios.create({
-        baseURL: 'http://localhost:5000',
+        baseURL: 'https://sinfbackend2.onrender.com',
         withCredentials: true,
       });
 
